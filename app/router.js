0d976e115b8f03f66e21e841df1199f33722b95c
John DiBaggio <jsantoku@gmail.com>
2015-06-26 19:58:38 -0500

    add home page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('index', {path: '/'});
     this.resource('todos', { path: '/todos' }, function() {
         this.route('active');
         this.route('complete');
