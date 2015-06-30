375e56d32563af4b14806fc8729d167c543368b4
jsantoku <jsantoku@gmail.com>
2015-06-29 21:16:58 -0500

    add api version

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,33 @@
 import DS from 'ember-data';
 
+var version = 1;
+
+function namespaceAndVersion (v) {
+    return "api" + "/v" + v;
+}
+
 export default DS.RESTAdapter.extend({
     host: "http://localhost:8000",
-    namespace: "api"
+    namespace: namespaceAndVersion(version)
     //headers: {}
 });
 
-//export default DS.FixtureAdapter.extend({});
+//export default DS.JsonApiAdapter.extend({
+//    host: "http://localhost:8000",
+//    namespace: namespaceAndVersion(version)
+//    //headers: {}
+//});
+
+//
+//import JsonApiAdapter from 'ember-json-api/json-api-adapter';
+//
+//export default JsonApiAdapter.extend({
+//    host: "http://localhost:8000",
+//    namespace: "api" + "/v1"
+//    //headers: {}
+//});
+//
+////import JsonApiAdapter from 'ember-json-api/json-api-adapter';
+////export default JsonApiAdapter;
+//
+////export default DS.FixtureAdapter.extend({});
